f7a5d665661158594124718106cc631cd0b51851
AlexeyOrlovsky <orlovskyiosdev@gmail.com>
2023-09-30 22:39:45 +0300

    Initial commit

---
+++ VideoEditorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoEditorApp
+//
+//  Created by Алексей Орловский on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
